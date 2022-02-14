793e1cbb82494fca988d692936d5b01e0d8e6330
Marc Hull <marc.hull@realvnc.com>
2022-02-14 08:37:23 +0000

    Remove gateway service.

--- gateway/src/main/java/com/realvnc/gateway/model/Permission.java
+++
@@ -1,5 +0,0 @@
-package com.realvnc.gateway.model;
-
-public enum Permission {
-    READ_MESSAGE, CREATE_MESSAGE, UPDATE_MESSAGE, DELETE_MESSAGE
-}
--- gateway/src/main/java/com/realvnc/gateway/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.realvnc.gateway.repository;
-
-import com.realvnc.gateway.model.User;
-
-import java.util.Optional;
-
-public interface UserRepository {
-    Optional<User> findUserByToken(String token);
-}
